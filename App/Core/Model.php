00e952532687978a377718795a45cb7fff51398d
THAMIRES GAUDENCIO AMÂNCIO <20272597@cfp127aluno.local>
2021-08-09 09:59:03 -0300

    aws heroku

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -8,9 +8,9 @@ class Model {
 
     public static function getConn(){
 
-        $host = $_ENV("database_host")
-        $user = $_ENV("database_user")
-        $password = $_ENV("database_pass")
+        $host = $_ENV("database_host");
+        $user = $_ENV("database_user");
+        $password = $_ENV("database_pass");
 
         if(!isset(self::$conexao)){
             self::$conexao = new \PDO("mysql:host=$host;port=3306;dbname=fastparking;", $user, $password);
